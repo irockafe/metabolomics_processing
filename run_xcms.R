bf7622462662ecfa63a928eac18a3da6552728e2
irockafe <isaac.rockafellow@gmail.com>
2017-12-07 22:40:08 -0500

    Updated run_xcms code to take IPO parameters from a tsv file. xcmsSet() calls are more specific

--- run_xcms.R
+++ run_xcms.R
@@ -5,6 +5,7 @@
 # Rscript run_xcms.R --preset uplc_orbitrap
 # Rscript run_xcms.R -sf summary_file.txt
 library(optparse)
+library(BiocParallel)
 # library(CAMERA) TODO!
 
 parser <- OptionParser()
@@ -27,7 +28,16 @@ parser <- add_option(parser, c('--output', '-o'), type='character',
 		     help=paste('Path to output processed
 				data to (single directory)'))
 
+parser <- add_option(parser, c('--cores'), type='integer',
+                     default = 4,
+                     help=paste('Optional, number of cores to 
+                        run things with'))
+
 args = parse_args(parser)
+
+# set number of cores to use
+print(paste('Number of cores ', args$cores))
+register(MulticoreParam(args$core))
 
 ### Parse a tab-delimited summary file
 char_to_numeric <- function(char) {
@@ -46,7 +56,8 @@ get_params <- function(path, char_to_numeric) {
    # It will accept vectors as space-delimited entries in the second column 
    # of the text file
    df <- read.table(path, sep='\t', header=FALSE, row.names=1,
-                    colClasses="character", blank.lines.skip=TRUE, fill=TRUE)
+                    colClasses="character", blank.lines.skip=TRUE, 
+                    fill=TRUE, strip.white=TRUE)
    print(df)
    print(dim(df))
    # There should only be one column of values
@@ -80,129 +91,119 @@ get_params <- function(path, char_to_numeric) {
     return(param_lst)
 }
 
-# Code to run xcms
-run_xcms = function(xcms_params, output_dir, data_dir,
-           # Optional params below. To set them, include
-           # a summary file 
-           detection_method='centWave', ppm=25, 
-           peak_width=c(10,60), prefilter=c(3,100),
-           # grouping parameters
-           bw=20, mzwid=0.025, minfrac=0, minsamp=5,
-           # retention-correction parameters
-           retcor_method='loess', missing=1, extra=1)
-           {
-    # INPUT - 
-    #    xcms_params - a named-list containing xcms parameters
-    #       It's origin is from a preset value or a summary file
-    #       parsed through the function get_params() 
-    # FUNCTION - Runs xcms with the supplied parameters
-    #    doing a group-retcor-group pass
-    # OUTPUT - A feature table, retcor deviation plot, and xcmsSet
-    #    objects after each grouping stage of xcms
-    #    output xcmsSet incase you want to repeat the group-retcor-group
-    #    again (TODO: not implemented)
-
-    # CALL - To call this function, use
-    # run_xcms(xcms_params, output_dir)
-    #     xcms_params will come from the get_params function 
-    # The parameters will be adopted from the summary file
-    # xcms_params
-
-
-    # override default params if present in parameters
-    # TODO: learn how to loop over entry-names 
-    if (!is.null(xcms_params$detection_method )){
-        detection_method<-xcms_params$detection_method
-    }
-    if (!is.null(xcms_params$ppm  )){
-        ppm <- xcms_params$ppm
-    }
-    if (!is.null(xcms_params$peak_width  )){
-       peak_width <- xcms_params$peak_width 
-    }
-    if (!is.null(xcms_params$prefilter  )){
-        prefilter <- xcms_params$prefilter
-    }
-    if (!is.null(xcms_params$bw  )){
-        bw <- xcms_params$bw
-    }
-    if (!is.null(xcms_params$mzwid )){
-        mzwid <- xcms_params$mzwid
-    }
-    if (!is.null(xcms_params$minfrac  )){
-        minfrac <- xcms_params$minfrac
-    }
-    if (!is.null(xcms_params$minsamp   )){
-        minsamp <- xcms_params$minsamp 
-    }
-    
-    if (!is.null(xcms_params$retcor_method   )){
-        retcor_method <- xcms_params$retcor_method
-    }
-    if (!is.null(xcms_params$missing   )){
-        missing <- xcms_params$missing
-    }
-    if (!is.null(xcms_params$extra   )){
-        extra <- xcms_params$extra
-    }
-    
-    
-    # escape spaces in data_dir path
-    xcms_feature_table = "xcms_result"
-    camera_feature_table = "xcms_camera_results.csv"
-    # nSlaves=0
-                  
-    ### change working directory to your files
-    ### TODO - this can probably be changed - xcms certainly has
-    ### a way to point to files
-    setwd(data_dir)
-    
-    #stop("Don't run xcms while you're debugging")
-
-    # Load packages
-    library(xcms)
-    library(snowfall)
-    library(BiocParallel)
-    # TODO - Learn to set the number of cores
-    #register(MulticoreParam(4))
-                             
-    # Detect peaks
-    xset <- xcmsSet(method=detection_method, ppm=ppm, prefilter=prefilter,
-                    peakwidth=peak_width, )
-    print("finished peak Detection!")
-    # Group peaks together across samples
-    # save the detected peaks in case downstream processing fails 
-    # and you have to go in manually to figure out parameters
-    # TODO - should the bw parameter here be changed depending
-    # on the platform used (i.e. bw=30 is default, but for uplc, 
-    # maybe the coarse grouping should be bw=15 or something)
-    xset <- group(xset)
-    print("finished first group command!")
-    print(xset)
-    saveRDS(xset, paste(output_dir, '/grouped.Rdata', sep=''))
-         
-    # Try to retention-correct
-    xset2 <- retcor(xset, method=retcor_method,
-                    family="s", plottype="m",
-                    missing=missing)
-    print("finished retcor!")
-     
-    # regroup after retention time correction
-    xset2 <- group(xset2, bw =bw, mzwid=mzwid,
-                          minfrac=0, minsamp=minsamp)
-    saveRDS(xset2, paste(output_dir, '/grouped_retcor.Rdata', sep=''))
-    print("finished second group command!")
-                                 
-    # Fill in peaks that weren't detected
-    xset3 <- fillPeaks(xset2)
-    print("Finished filling peaks!")
-    print(xset3)
-    # Move back to output_dir and write out your feature table
-    setwd(output_dir)
-    xcms_peaklist = peakTable(xset3, filebase=xcms_feature_table)
-    #write.csv(xcms_peaklist, paste(xcms_feature_table, sep='_'))
-    print("Finished xcms!")
-    }
+# Run xcms with parameters from a file generated by IPO
+run_xcms = function(xcms_params, output_dir, data_dir)
+{
+  # INPUT - 
+  #    xcms_params - a named-list containing xcms parameters
+  #       It's origin is from a preset value or a summary file
+  #       parsed through the function get_params() 
+  # FUNCTION - Runs xcms with the supplied parameters
+  #    doing a group-retcor-group pass
+  # OUTPUT - A feature table, retcor deviation plot, and xcmsSet
+  #    objects after each grouping stage of xcms
+  #    output xcmsSet incase you want to repeat the group-retcor-group
+  #    again (TODO: not implemented)
+  
+  # CALL - To call this function, use
+  # run_xcms(xcms_params, output_dir)
+  #     xcms_params will come from the get_params function 
+  # The parameters will be adopted from the summary file
+  # xcms_params
+  
+  
+  # escape spaces in data_dir path
+  xcms_feature_table = "xcms_result"
+  camera_feature_table = "xcms_camera_results.csv"
+  # nSlaves=0
+  
+  ### change working directory to your files
+  ### TODO - this can probably be changed - xcms certainly has
+  ### a way to point to files
+  setwd(data_dir)
+  print(getwd())
+  
+  #stop("Don't run xcms while you're debugging")
+  
+  # Load packages
+  # library(xcms)
+  library(snowfall)
+  library(BiocParallel)
+  # TODO - Learn to set the number of cores
+  #register(MulticoreParam(4))
+  
+  # Detect peaks
+  xset <- xcms::xcmsSet(
+    method = "centWave",
+    peakwidth = xcms_params$peak_width,
+    ppm         = xcms_params$ppm,
+    noise       = xcms_params$noise,
+    snthresh    = xcms_params$snthresh,
+    mzdiff      = xcms_params$mzdiff,
+    prefilter   = xcms_params$prefilter,
+    mzCenterFun = xcms_params$mzCenterFun,
+    integrate   = xcms_params$integrate
+  )
+  
+  print("finished peak Detection!")
+  # Group peaks together across samples
+  # save the detected peaks in case downstream processing fails 
+  # and you have to go in manually to figure out parameters
+  # TODO - should the bw parameter here be changed depending
+  # on the platform used (i.e. bw=30 is default, but for uplc, 
+  # maybe the coarse grouping should be bw=15 or something)
+  xset <- xcms::group(
+    xset, 
+    method  = "density", 
+    bw      = xcms_params$bw * 2, # First binning step should be generous 
+    mzwid   = xcms_params$mzwid, 
+    minfrac = xcms_params$minfrac, 
+    minsamp = xcms_params$minsamp, 
+    max     = xcms_params$max
+  )
+  print("finished first group command!")
+  print(xset)
+  saveRDS(xset, paste(output_dir, '/grouped.Rdata', sep=''))
+  
+  # Try to retention-correct
+  xset2 <- xcms::retcor(xset, 
+                        method         = xcms_params$retcor_method,
+                        plottype       = 'deviation', 
+                        distFunc       = xcms_params$distFunc,
+                        profStep       = xcms_params$profStep, 
+                        center         = xcms_params$center, 
+                        response       = xcms_params$response, 
+                        gapInit        = xcms_params$gapInit, 
+                        gapExtend      = xcms_params$gapExtend,
+                        factorDiag     = xcms_params$factorDiag,
+                        factorGap      = xcms_params$factorGap, 
+                        localAlignment = xcms_params$localAlignment)
+  
+  print("finished retcor!")
+  
+  # regroup after retention time correction
+  xset2 <- xcms::group(
+    xset2, 
+    method  = "density", 
+    bw      = xcms_params$bw, # Second binning step should be more strict 
+    mzwid   = xcms_params$mzwid, 
+    minfrac = xcms_params$minfrac, 
+    minsamp = xcms_params$minsamp, 
+    max     = xcms_params$max
+  )
+  saveRDS(xset2, paste(output_dir, '/grouped_retcor.Rdata', sep=''))
+  print("finished second group command!")
+  
+  # Fill in peaks that weren't detected
+  xset3 <- xcms::fillPeaks(xset2)
+  print("Finished filling peaks!")
+  print(xset3)
+  # Move back to output_dir and write out your feature table
+  setwd(output_dir)
+  xcms_peaklist = xcms::peakTable(xset3, filebase=xcms_feature_table)
+  #write.csv(xcms_peaklist, paste(xcms_feature_table, sep='_'))
+  print("Finished xcms!")
+}
 
 print(args$summaryfile)
 print(is.null(args$summaryfile))
@@ -235,9 +236,14 @@ if (is.null(args$data)) {
 	That way xcms can find it')
 }
 
+# TODO - make os-agnostic (replace backslashes) 
 print('Output directory!')
 print(args$output)
+local_path = system('git rev-parse --show-toplevel', intern=TRUE)
+output_path = paste(local_path, args$output, sep='/')
+data_path = paste(local_path, args$data, sep='/')
 # Done - test for full summary file
 # Done - fails for missing items - test for partial summary file
 # Done - test for summary file
-system.time(debug(run_xcms(xcms_params, args$output, args$data)))
+
+system.time(debug(run_xcms(xcms_params, output_path, data_path)))
