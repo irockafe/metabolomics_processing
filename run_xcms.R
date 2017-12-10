00c739e32e157e43b1c135b313edf1fea3710497
irockafe <isaac.rockafellow@gmail.com>
2017-12-10 14:40:00 -0500

    fix fxn name

--- run_xcms.R
+++ run_xcms.R
@@ -228,7 +228,7 @@ if (is.character(args$summaryfile)){
 }
 
 
-function set_absolute_path(local_path, given_path) {
+set_absolute_path <- function(local_path, given_path) {
   # Decide whether or not user gave you have an absolute path
   # if the known local path (non-git path) if present
   # then assume it's absolute
@@ -236,9 +236,9 @@ function set_absolute_path(local_path, given_path) {
   # If local path is in the path given on command line, return 
   # command line path. Otherwise, add the local path to it
   if (grepl(local_path, given_path)){
-    return given_path
+    return(given_path)
   } else {
-    return paste(local_path, given_path, sep='/')
+    return(paste(local_path, given_path, sep='/'))
   }
 
 }
