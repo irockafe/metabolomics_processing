c6944e8ef0c7eb537e8b28754d92665b06a80897
Isaac Rockafellow <isaac.rockafellow@gmail.com>
2017-12-14 21:17:47 +0000

    checking how conditionals work in pydoit. They're normal

--- references/pydoit_notes/dodo.py
+++ references/pydoit_notes/dodo.py
@@ -3,9 +3,13 @@ DOIT_CONFIG = {'check_file_uptodate': 'timestamp_newer',
 
 
 def task_touch():
-    yield {
-        'targets': ['poop.txt'],
-        'file_dep': ['shit.txt'],
-        'actions': ['touch poop.txt feces.txt'],
-        'name': 'touch poop feces'
-    }
+    for i in range(1,7):
+        print i
+        if i % 2 == 0:
+            print i
+            yield {
+                'targets': ['poop_%s.txt' % i],
+                'file_dep': ['shit.txt'],
+                'actions': ['touch poop_%s.txt feces_%s.txt' % (i,i)],
+                'name': 'touch poop feces %s' % i
+                   }
