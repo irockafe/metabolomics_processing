45624023bef9eec4b0023ab4665920be7edd7c99
root <root@mass-spec.us-east1-b.c.school-segregation.internal>
2018-12-11 02:01:35 +0000

    added gcloud support for checking if buckets exist

--- src/project_fxns/project_fxns.py
+++ src/project_fxns/project_fxns.py
@@ -1,10 +1,11 @@
 # Utility functions used in more than one script
 import os
 import yaml
+import subprocess
 
 
 class Storage():
-    def __init__(self, study):
+    def __init__(self):
         # get cloud provider and bucket path
         path = '/home/user_input/user_settings.yml'
         yml = yaml.load(file(path, 'r'))
@@ -25,17 +26,23 @@ class Storage():
                 s3_path=self.storage_path, path=path)
                 )
             check_bucket = subprocess.call(ls_s3, shell=True)
-            # TODO probably more direct/better way to do this
             if check_bucket == 0:
                 return True
             else:
                 return False
 
         if self.cloud_provider == 'google':
-            #TODO
-            pass
+            ls_gcloud = ("gsutil ls {gcloud}/{path}".format(
+                gcloud=(self.storage_path), path=path)
+                )
+            # returns 0 if directory exists, 1 otherwise
+            check_bucket = subprocess.call(ls_gcloud, shell=True)
+            if check_bucket == 0:
+                return True
+            else:
+                return False
 
-    def sync_to_storage(self, local_path, bucket_path):
+    def sync_to_storage(self, local_path):
         # syncs local to storage bucket
 
         # if local_path is absolute, i.e. /home/Data/raw/poop.data
@@ -46,17 +53,23 @@ class Storage():
             relative_path = local_path
 
         if self.cloud_provider == 'amazon':
-           sync = ("nohup aws s3 sync '{local_path}' ".format(
-               local_path=local_path)
-               +
+            sync = ("nohup aws s3 sync '{local_path}' ".format(
+                    local_path=local_path)
+                +
                "'{s3_path}/{path}'".format(s3_path=self.storage_path,
                    path=relative_path)
                )
-        if self.cloud_provider == 'google':
-            #TODO`
-            pass
+            logging.info("Sync-ing to cloud with:\n%s" % sync)
+            subprocess.call(sync, shell=True)
 
-    def sync_to_local(self, local_path, bucket_path):
+        if self.cloud_provider == 'google':
+            sync = ('nohup gsutil rsync -r {path} {storage}/{path}'.format(
+                path=local_path, storage=self.storage_path)
+                )
+            logging.info("Sync-ing to cloud with:\n%s" % sync)
+            subprocess.call(sync, shell=True)
+
+    def sync_to_local(self, local_path):
         # syncs sotrage to local
         # syncs local to storage bucket
 
@@ -74,10 +87,15 @@ class Storage():
                 +
                 "'{local_path}'".format(local_path=local_path)
                 )
+            logging.info("Sync-ing to local with:\n%s" % sync)
             subprocess.call(sync, shell=True)
 
         if self.cloud_provider == 'google':
-            pass
+            sync = ('nohup gsutil rsync -r {storage}/{path} {path}'.format(
+                storage=self.storage_path, path=local_path)
+                )
+            logging.info("Sync-ing to local with:\n%s" % sync)
+            subprocess.call(sync, shell=True)
 
 
 #
