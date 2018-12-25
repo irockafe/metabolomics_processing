cf428f611139f0a97fabbb9a84e497b5f75a4e17
root <root@mass-spec.us-east1-b.c.school-segregation.internal>
2018-12-25 16:10:45 +0000

    made bucket path better

--- src/project_fxns/project_fxns.py
+++ src/project_fxns/project_fxns.py
@@ -2,6 +2,7 @@
 import os
 import yaml
 import subprocess
+import logging
 
 
 class Storage():
@@ -10,20 +11,19 @@ class Storage():
         path = '/home/user_input/user_settings.yml'
         yml = yaml.load(file(path, 'r'))
         self.cloud_provider = yml['cloud_provider']
-        self.storage_path = yml['storage']
-        # ensure consistent slashes
-        if self.storage_path[-1] == '/':
-            self.storage_path = self.storage_path[:-1]
+        self.cloud_url_base = yml['storage']
    
     def bucket_exists(self, path):
         # checks if the path to file exists
         # return bool
         # make sure they're giving you relative paths
-        if path[0] == '/':
-            path = path[1:]
+        #if path[0] == '/':
+        #    path = path[1:]
+        # TODO this assumes linux url joining
+        cloud_url = os.path.join(self.cloud_url_base, path)
         if self.cloud_provider == 'amazon':
-            ls_s3 = ("nohup aws s3 ls  '{s3_path}/{path}'".format(
-                s3_path=self.storage_path, path=path)
+            ls_s3 = ("nohup aws s3 ls  '{aws_path}'".format(
+                aws_path = cloud_url)
                 )
             check_bucket = subprocess.call(ls_s3, shell=True)
             if check_bucket == 0:
@@ -31,9 +31,9 @@ class Storage():
             else:
                 return False
 
-        if self.cloud_provider == 'google':
-            ls_gcloud = ("gsutil ls {gcloud}/{path}".format(
-                gcloud=(self.storage_path), path=path)
+        elif self.cloud_provider == 'google':
+            ls_gcloud = ("gsutil ls {gcloud_path}".format(
+                gcloud_path = cloud_url)
                 )
             # returns 0 if directory exists, 1 otherwise
             check_bucket = subprocess.call(ls_gcloud, shell=True)
@@ -42,60 +42,65 @@ class Storage():
             else:
                 return False
 
-    def sync_to_storage(self, local_path):
+    def sync_to_storage(self, local_path, storage_path):
+        # Local path is the path where data is
+        # storage_path is where you want it in the cloud
         # syncs local to storage bucket
-
-        # if local_path is absolute, i.e. /home/Data/raw/poop.data
-        # drop the /home/
-        if '/home/' in local_path:
-            relative_path = local_path.replace('/home/', '')
-        else:
-            relative_path = local_path
-
+        cloud_url = os.path.join(self.cloud_url_base, storage_path)
         if self.cloud_provider == 'amazon':
-            sync = ("nohup aws s3 sync '{local_path}' ".format(
-                    local_path=local_path)
-                +
-               "'{s3_path}/{path}'".format(s3_path=self.storage_path,
-                   path=relative_path)
-               )
+            sync = "nohup aws s3 sync '{local_path}' '{s3_path}'".format(
+                    local_path=local_path, s3_path=cloud_url)
+                          
+               
             logging.info("Sync-ing to cloud with:\n%s" % sync)
             subprocess.call(sync, shell=True)
 
         if self.cloud_provider == 'google':
-            sync = ('nohup gsutil rsync -r {path} {storage}/{path}'.format(
-                path=local_path, storage=self.storage_path)
-                )
+            sync = "nohup gsutil rsync -r '{local}' '{gcloud_path}'".format(
+                      local=local_path, 
+                      gcloud_path=cloud_url) 
+                      
             logging.info("Sync-ing to cloud with:\n%s" % sync)
             subprocess.call(sync, shell=True)
 
-    def sync_to_local(self, local_path):
+    def sync_to_local(self, local_path, storage_path):
         # syncs sotrage to local
-        # syncs local to storage bucket
-
-        # if local_path is absolute, i.e. /home/Data/raw/poop.data
-        # drop the /home/
-        if '/home/' in local_path:
-            relative_path = local_path.replace('/home/', '')
-        else:
-            relative_path = local_path
-
+        # syncs local_path to storage_path
+        cloud_url = os.path.join(self.cloud_url_base, storage_path)
         if self.cloud_provider == 'amazon':
-            sync = ("nohup aws s3 sync '{s3}/{path}' ".format(
-                s3=self.storage_path, 
-                path=relative_path)
-                +
-                "'{local_path}'".format(local_path=local_path)
+            sync = ("nohup aws s3 sync '{s3_path}' '{local_path}'".format(
+                s3_path=self.cloud_url_base, 
+                local_path=local_path)
                 )
             logging.info("Sync-ing to local with:\n%s" % sync)
             subprocess.call(sync, shell=True)
 
         if self.cloud_provider == 'google':
-            sync = ('nohup gsutil rsync -r {storage}/{path} {path}'.format(
-                storage=self.storage_path, path=local_path)
+            sync = ("nohup gsutil rsync -r '{gcloud_path}' '{local_path}'".format(
+                gcloud_path=cloud_url,
+                local_path=local_path)
                 )
             logging.info("Sync-ing to local with:\n%s" % sync)
             subprocess.call(sync, shell=True)
 
 
-#
+def class_names(study, assay, 
+        a_path, s_path, yaml_path):
+    # Goal: output mapping b/t sample name, 
+    #     MS Assay name, class labels
+    #     Then output this info to its own file,
+    #     since I'm guessint the user-input
+    #     will make automating this a huge pain
+    # paths to a_*.txt and s_*.txt 
+    # Given yaml file with the correct class name
+    # column listed, output a mapping b/t 
+    # assay names and sample names and classes
+    yml = yaml.load(file(yaml_path, 'r'))
+    col_name = yml[study][assay]['class_label_column']
+    a = (pd.read_csv(a_path, sep='\t')
+            .set_index('Sample Name'))
+    s = (pd.read_csv(a_path, sep='\t')
+            .set_index('Sample Name'))
+    mapping = pd.concat([a['MS Assay Name', 
+        'Raw Spectral Data File'], s[col_name]]) 
+    return mapping
