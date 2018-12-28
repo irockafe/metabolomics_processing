b0101367d5d4b9793173785f95a1c5d8b0710337
irockafe <isaac.rockafellow@gmail.com>
2018-12-27 19:41:29 -0500

    minor changes to unimportant things

--- references/pydoit_notes/dodo.py
+++ references/pydoit_notes/dodo.py
@@ -1,15 +1,18 @@
 DOIT_CONFIG = {'check_file_uptodate': 'timestamp_newer',
                'verbosity': 2}
 
+#DOIT_CONFIG = {'check_file_uptodate': 'timestamp',
+#               'verbosity': 2}
+
 
 def task_touch():
-    for i in range(1,7):
+    for i in range(1, 7):
         print i
         if i % 2 == 0:
             print i
             yield {
-                'targets': ['poop_%s.txt' % i],
                 'file_dep': ['shit.txt'],
-                'actions': ['touch poop_%s.txt feces_%s.txt' % (i,i)],
-                'name': 'touch poop feces %s' % i
+                'targets': ['poop_%s.txt' % i],
+                'actions': ['touch poop_%s.txt' % (i)],
+                'name': 'touch poop %s' % i
                    }
